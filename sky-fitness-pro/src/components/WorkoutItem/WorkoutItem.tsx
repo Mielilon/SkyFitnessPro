fbeea38b781043d2bdd4bcc0b4942181e2f8cc6f
ZakhTania <tatizarova@gmail.com>
2024-05-26 09:38:13 +0300

    меняющийся цвет иконки

--- sky-fitness-pro/src/components/WorkoutItem/WorkoutItem.tsx
+++ sky-fitness-pro/src/components/WorkoutItem/WorkoutItem.tsx
@@ -30,7 +30,6 @@ export default function WorkoutItem({
           className="p-3 pl-[34px] blok rounded-[30px]  font-roboto-400 text-[18px] md:text-[24px]  leading-[110%]"
         >
           {workoutName}
-          {isDone && (
           <svg
             className="absolute top-[22px] left-2"
             width="20"
@@ -43,27 +42,7 @@ export default function WorkoutItem({
               fill-rule="evenodd"
               clip-rule="evenodd"
               d="M10 20C15.5228 20 20 15.5228 20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20ZM9.91339 14.1459L15.4134 7.64594L13.8866 6.35406L9.1373 11.9669L6.40258 8.8415L4.89742 10.1585L8.39742 14.1585C8.58922 14.3777 8.86704 14.5024 9.15829 14.5C9.44953 14.4976 9.72525 14.3683 9.91339 14.1459Z"
-              fill="#BCEC30"
-            />
-          </svg>
-        )}
-          <svg
-            className="absolute top-[22px] left-2"
-            width="20.000000"
-            height="20.000000"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs />
-            <circle
-              id="Ellipse 3845"
-              cx="10.000000"
-              cy="10.000000"
-              r="9.500000"
-              stroke="#000000"
-              strokeOpacity="1"
-              strokeWidth="1.000000"
+              fill={isDone ? "#BCEC30" : "#D0CECE"}
             />
           </svg>
         </label>
